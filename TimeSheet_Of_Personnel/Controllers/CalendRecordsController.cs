b8da596c260708a7e90002d2e3392a19412f6de9
Hereigo <Hereigo@online.ua>
2016-09-12 14:42:37 +0300

    Change Object To Jugged Array when send 2 View

--- TimeSheet_Of_Personnel/Controllers/CalendRecordsController.cs
+++ TimeSheet_Of_Personnel/Controllers/CalendRecordsController.cs
@@ -16,6 +16,71 @@ namespace TimeSheet_Of_Personnel.Controllers
 
         // public ActionResult SeedFirst
 
+        public ActionResult MonthView_2()
+        {
+            // TODO:
+            // REFACTORE ME !!!!!!!
+            // REFACTORE ME !!!!!!!
+
+            int currYear = 2016;
+            int currMonth = 7;
+
+            ViewBag.MonthDate = new DateTime(currYear, currMonth, 1);
+
+            DateTime firstDayOfMonth = new DateTime(currYear, currMonth, 01);
+            DateTime lastDayOfMonth = new DateTime(currYear, currMonth, 31);
+
+            List<CalendRecord> actualCalRecords = (from r in db.CalendRecords
+                                                   from e in db.Employees
+                                                   where (e.WorkEnd == null || e.WorkEnd >= firstDayOfMonth) &&
+                                                   r.CalendRecordName >= firstDayOfMonth &&
+                                                   r.CalendRecordName <= lastDayOfMonth
+                                                   select r).ToList();
+
+            List<Employee> actualEmployees = (from rec in actualCalRecords select rec.Employee).Distinct().ToList();
+
+            int daysInMon = DateTime.DaysInMonth(currYear, currMonth);
+            // (daysInCurrMonth) + 1.Num + 2.Name + 3.Position + 4.IsWoman + 5.TimeSheetNum
+            int firstColsShift = 5;
+            // 1.Фактично відпрац. дні, 2.відпустка, 3.відрядження, 4.відгул, 
+            // 5.неявк з незяс.прич., 6.підвищ.кваліфік., 7.хвороба, 8.Вихідні, святкові дні
+            int lastColsShift = 8;
+
+            int rowsCnt = actualEmployees.Count();
+
+            string[,] rows = new string[rowsCnt, (daysInMon + firstColsShift + lastColsShift)];
+
+            // ADD ROWS :
+            for (int row = 0; row < rowsCnt; row++)
+            {
+                // FILL ROW WITH - "8"
+                for (int col = 0; col < firstColsShift + daysInMon + lastColsShift; col++)
+                {
+                    rows[row, col] = "8";
+                }
+
+                rows[row, 0] = row.ToString();
+                rows[row, 1] = actualEmployees[row].EmployeeName;
+                rows[row, 2] = actualEmployees[row].EmployPosition;
+                rows[row, 3] = actualEmployees[row].IsAWoman ? "+" : "";
+                rows[row, 4] = actualEmployees[row].EmployeeID.ToString();
+
+                foreach (CalendRecord rec in actualCalRecords.Where(e => e.EmployeeID == actualEmployees[row].EmployeeID))
+                {
+                    // COLUMN WITH NUMBER OF DAY IN MONTH (with shift) = CALEND.RECORD WITH THE SAME DAY IN CURRENT MONTH
+                    rows[row, rec.CalendRecordName.Day + firstColsShift - 1] = rec.DayType.SymbolName;
+                }
+            }
+
+            ViewBag.calendMatrix = rows;
+            ViewBag.rows = rowsCnt;
+            ViewBag.columns = daysInMon + firstColsShift + lastColsShift;
+
+            return View();
+        }
+
+
+
         public ActionResult MonthView()
         {
             // TODO:
@@ -56,7 +121,7 @@ namespace TimeSheet_Of_Personnel.Controllers
                 List<CalendRecord> daysForCurrEmp = (from d in calendRecords
                                                      where
                             d.Employee.EmployeeName == emp.EmployeeName
-                                                  select d).ToList();
+                                                     select d).ToList();
 
 
                 foreach (var day in daysForCurrEmp)

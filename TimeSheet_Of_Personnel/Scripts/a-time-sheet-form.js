450c6569123691a2a62b55d683a267c500e0956c
Hereigo <Hereigo@online.ua>
2016-10-11 12:20:52 +0300

    Add ShortDay-Form Confirmation&Check

--- TimeSheet_Of_Personnel/Scripts/a-time-sheet-form.js
+++ TimeSheet_Of_Personnel/Scripts/a-time-sheet-form.js
@@ -1,17 +1,26 @@
 ﻿function showForm() {
     document.getElementById('formBox').style.display = 'block';
 }
+
 function hideForm() {
     document.getElementById('formBox').style.display = 'none';
 }
+
 function checkForm() {
     var dateField = document.getElementById('shortDayDate').value;
+    var weekDays = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "Пятниця", "Субота"];
     if (isValidDate(dateField)) {
-        document.getElementById('postForm').submit();
+        var bits = dateField.split('/');
+        var formatedDate = bits[2] + '/' + bits[1] + '/' + bits[0];
+        var date = new Date(formatedDate);
+        if (confirm("Встановити скорочений день " + dateField + " (" + weekDays[date.getDay()] + ")?")) {
+            document.getElementById('postForm').submit();
+        }
     } else {
         alert("Не коректний формат дати!");
     }
 }
+
 function isValidDate(s) {
     var bits = s.split('/');
     var y = bits[2], m = bits[1], d = bits[0];
